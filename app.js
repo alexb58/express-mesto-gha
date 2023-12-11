b05e1d406b202ed54651c4a3214f4175e86b9058
Nikita <invginaku@yandex.ru>
2023-12-11 22:10:35 +0300

    [Project-12] initial commit

---
+++ app.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const { PORT = 3000 } = process.env;
+const router = require('./routes/index');
+
+const app = express();
+const dbUrl = 'mongodb://localhost:27017/mestodb';
+
+mongoose.connect(dbUrl, {
+  useNewUrlParser: true,
+});
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Middleware для установки фиктивного пользователя в запросе
+app.use((req, res, next) => {
+  req.user = {
+    _id: '6439cc795a2a1fb8080e67ce',
+  };
+  next();
+});
+
+app.use(router);
+
+app.listen(PORT, () => {
+  console.log(`App listening on port ${PORT}`);
+});
---
+++ models/user.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose');
+
+const nameOptions = {
+  type: String,
+  required: [true, 'не передано имя пользователя'],
+  minlength: [2, 'длина имени пользователя должна быть не менее 2 символов'],
+  maxlength: [30, 'длина имени пользователя должна быть не более 30 символов']
+};
+
+const aboutOptions = {
+  type: String,
+  required: [true, 'не передано информации о пользователе'],
+  minlength: [2, 'длина информации о себе должна быть не менее 2 символов'],
+  maxlength: [30, 'длина информации о себе должна быть не более 30 символов']
+};
+
+const avatarOptions = {
+  type: String,
+  required: [true, 'не передана ссылка на аватар пользователя']
+};
+
+const userSchema = new mongoose.Schema(
+  {
+    name: nameOptions,
+    about: aboutOptions,
+    avatar: avatarOptions,
+  },
+  { versionKey: false }
+);
+
+module.exports = mongoose.model('user', userSchema);
